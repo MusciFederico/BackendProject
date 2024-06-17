3915418758c655271522cdd7e9b47e9afa321fc1
Federico Musci <muscifede@gmail.com>
2024-06-17 18:29:20 -0300

    railway test

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,6 +4,7 @@ import logger from "./logger/logger.factory.js";
 
 const dbConnection = async () => {
     try {
+        logger.INFO("environment", env);
         await connect(env.DB_LINK);
         logger.INFO("db connected",);
     } catch (error) {
